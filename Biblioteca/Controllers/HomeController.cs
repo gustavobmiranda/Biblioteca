754e6cad0fc536150d256c9c52d281b346eaacd4
gustavobmiranda <gustavobmd3@hotmail.com>
2023-05-30 09:20:31 -0300

    Adicionar arquivos de projeto.

---
+++ Biblioteca/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Biblioteca.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.OpcaoMenu = 1;
+            return View();
+        }
+
+        public IActionResult Clientes()
+        {
+            ViewBag.OpcaoMenu = 2;
+            return View();
+        }
+
+        public IActionResult Livros()
+        {
+            ViewBag.OpcaoMenu = 3;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
